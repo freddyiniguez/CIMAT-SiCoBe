9d514f6fd354a583860d0498f67a3cd198d60737
jmanem282 <jmanem282@yahoo.com>
2015-06-07 15:49:55 -0500

    Vistas Año fiscal, Alta bebeficiario y Modificar beneficioario

---
+++ view/altafiscal.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>SiCoBe</title>
+  <meta name="description" content="Sistema de Control de Beneficiarios para mejorar el control de los recursos asignados.">
+  <link href="../css/style.css" rel="stylesheet" type="text/css">
+  <link href="../css/bootstrap.css" rel="stylesheet" type="text/css">
+  <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="../css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
+  <link href="../css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div class="main">
+<div class="page">
+<div class="header">
+<div class="header-img">
+<h1>SiCoBe</h1>
+<p>Sistema de Control de Beneficiarios</p>
+</div>
+<div class="menu">
+<ul>
+  <li><a href="../index.php">Home</a></li>
+  <li class="item_selected"><a href="#">Dependencias</a></li>
+  <li><a href="programas.php">Programas</a></li>
+  <li><a href="beneficiarios.php">Beneficiarios</a></li>
+  <li><a href="about.php">Acerca de</a></li>
+</ul>
+</div>
+</div>
+<div class="content">
+<div class="left-panel">
+<div class="left-panel-in">
+<h2 class="title">Alta de Año Fiscal</h2>
+    <form class="form-horizontal">
+<!-- Text input-->
+<div class="control-group">
+  <br>
+  <label class="control-label" for="txtaño">Año:</label>
+  <div class="controls">
+    <input id="txtaño" name="txtaño" type="text" placeholder="" class="input-medium" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="txtpresupuesto">Presupuesto:</label>
+  <div class="controls">
+    <input id="txtpresupuesto" name="txtpresupuesto" type="text" placeholder="" class="input-medium" required="">
+    
+  </div>
+</div>
+
+<!-- Button (Double) -->
+<div class="control-group">
+  <label class="control-label" for="btnRegistrar"></label>
+  <div class="controls">
+    <button id="btnRegistrar" name="btnRegistrar" class="btn btn-success">Registrar</button>
+    <button id="btnCancelar" name="btnCancelar" class="btn btn-danger">Cancelar</button>
+  </div>
+</div>
+</form>
+</div>
+</div>
+<div class="right-panel">
+<div class="right-panel-in">
+
+<h3>Categories</h3>
+<ul>
+  <li><a href="#">Link item 1<br>
+    </a></li>
+  <li><a href="index.html#">Link item 2<br>
+    </a></li>
+  <li><a href="index.html#">Link item 3<br>
+    </a></li>
+  <li><a href="index.html#">Link item 4<br>
+    </a></li>
+  <li><a href="index.html#">Link item 5<br>
+    </a></li>
+  <li><a href="index.html#">Link item 6<br>
+    </a></li>
+</ul>
+<h3>Blogroll</h3>
+<ul>
+  <li><a href="#">Blogroll link 1<br>
+    </a></li>
+  <li><a href="index.html#">Blogroll link 2<br>
+    </a></li>
+  <li><a href="index.html#">Blogroll link 3<br>
+    </a></li>
+  <li><a href="index.html#">Blogroll link 4<br>
+    </a></li>
+  <li><a href="index.html#">Blogroll link 5<br>
+    </a></li>
+  <li><a href="index.html#">Blogroll link 6<br>
+    </a></li>
+</ul>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</div>
+</div>
+</div>
+<!-- FOOTER -->
+<div class="footer">
+<p>&copy; Copyright 2015. Creado por <a target="_blank" href="http://www.economia.gob.mx/">Secretaría de Economía</a> para el <a target="_blank" href="http://www.zacatecas.gob.mx/">Estado de Zacatecas</a>
+</p>
+</div>
+</div>
+</div>
+
+<a href="http://www.htmltemplates.net">
+<img src="images/design_img.gif" class="copyright" alt="HTML Templates"></a>
+
+</body>
+</html>
