9e3dbc20342f13bd1153d92c0634ecc3d6b145fa
Freddy Íñiguez López <iniguez.devt@gmail.com>
2015-06-08 02:52:03 -0500

    Clase para cargar menú dinamicamente

---
+++ classes/menu.php
@@ -0,0 +1,29 @@
+<nav>
+    <ul>
+      <li class="item_selected"><a href="#">Home</a></li>
+      <li>
+        <a href="#">Dependencias</a>
+        <ul class="fallback">
+          <li><a href="view/altadependencia.php">Agregar</a></li>
+          <li><a href="view/modificardependencia.php">Editar</a></li>
+          <li><a href="view/consultardependencia.php">Mostrar</a></li>
+        </ul>
+      </li>
+    <li>
+      <a href="#">Programas</a>
+      <ul class="fallback">
+        <li><a href="view/altaprograma.php">Agregar</a></li>
+        <li><a href="view/consultarprograma.php">Consultar</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">Beneficiarios</a>
+      <ul class="fallback">
+        <li><a href="view/altabeneficiario.php">Agregar</a></li>
+        <li><a href="view/buscarbeneficiario.php">Buscar</a></li>
+        <li><a href="view/historialbeneficiario.php">Historial</a></li>
+      </ul>
+    </li>
+    <li><a href="view/about.php">Acerca de</a></li>
+    </ul>
+  </nav>
